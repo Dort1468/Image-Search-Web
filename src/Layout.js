d58e26dd73173edd74810449ee76acc1d3093192
Billy Chen <billy61307aaa@gmail.com>
2024-09-06 21:40:19 +0800

    + 完成初步網站搜尋功能 - 設計美化 - 關於頁面撰寫

---
+++ src/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">首頁</Link>
+          </li>
+          <li>
+            <Link to="/about">關於這網站</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
